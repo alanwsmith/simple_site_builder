17fe1c206c433a2be455e4969a38fc55bcfcfd2d
Alan Smith <commit@alanwsmith.com>
2025-06-08 09:56:25 -0400

    Moeved get_files_in_dir to helpers

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,48 @@
 use anyhow::{Result, anyhow};
 use itertools::Itertools;
+use std::fs;
 use std::path::PathBuf;
 use walkdir::WalkDir;
+
+pub fn get_files_in_dir(
+    dir: &PathBuf,
+    with: Option<Vec<&str>>,
+    without: Option<Vec<&str>>,
+) -> Result<Vec<PathBuf>> {
+    Ok(fs::read_dir(dir)?
+        .into_iter()
+        .filter_map(|path| path.ok())
+        .map(|path| path.path().to_path_buf())
+        .filter(|path| path.is_file())
+        .filter_map(|path| match path.strip_prefix(dir) {
+            Ok(p) => Some(p.to_path_buf()),
+            Err(_) => None,
+        })
+        .filter(|path| !path.display().to_string().starts_with("."))
+        .filter(|path| {
+            if let Some(with) = &with {
+                if let Some(ext) = path.extension() {
+                    with.contains(&ext.to_str().unwrap())
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        })
+        .filter(|path| {
+            if let Some(without) = &without {
+                if let Some(ext) = path.extension() {
+                    !without.contains(&ext.to_str().unwrap())
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        })
+        .collect())
+}
 
 pub fn get_files_in_tree(
     dir: &PathBuf,
