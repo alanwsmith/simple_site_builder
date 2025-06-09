227c54c62840706ddd39bb896173368c0f0bbb83
Alan Smith <commit@alanwsmith.com>
2025-06-09 00:34:50 -0400

    Update to script

--- src/defaults/scripts/0001-make-misc-data.py
+++ src/defaults/scripts/0001-make-misc-data.py
@@ -3,19 +3,16 @@
 import json
 from datetime import datetime
 
-def write_json_to_file(data, file):
-    with open(file, 'w', encoding='utf-8') as _out:
-        json.dump(data, _out, sort_keys=True, indent=4)
-
 now = datetime.now()
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-d = {
-        "updated": current_time
-    }
+data = {
+    "updated": current_time
+}
 
+path = "../data/auto.json"
+with open(path, 'w', encoding='utf-8') as _out:
+    json.dump(data, _out, sort_keys=True, indent=4)
 
-file_path = "../data/auto.json"
-write_json_to_file(d, file_path)
 
 
