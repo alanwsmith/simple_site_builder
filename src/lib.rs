50087e19eecd670694c984e7702c88a288291d6c
Alan Smith <commit@alanwsmith.com>
2025-09-02 13:46:30 -0400

    Starting v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-pub mod helpers;
-pub mod renderer;
-pub mod run_builder;
-pub mod run_scripts;
-pub mod run_server;
-pub mod run_watcher;
-pub mod site;
+pub mod builder;
+pub mod server;
+pub mod watcher;
---
+++ src_original/lib.rs
@@ -0,0 +1,7 @@
+pub mod helpers;
+pub mod renderer;
+pub mod run_builder;
+pub mod run_scripts;
+pub mod run_server;
+pub mod run_watcher;
+pub mod site;
