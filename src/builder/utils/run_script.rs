d96f4c82670789010918f2798c02623c63bac0a4
Alan Smith <commit@alanwsmith.com>
2025-09-25 14:46:53 -0400

    Removed execution set from run_script and watching the script dir for changes.

--- src/builder/utils/run_script.rs
+++ src/builder/utils/run_script.rs
@@ -1,25 +1,28 @@
 use anyhow::Result;
-use std::fs;
 use std::io::Write;
-use std::os::unix::fs::PermissionsExt;
 use std::process::{Command, Stdio};
 
 pub fn run_script(
   script: &String,
   data: &[u8],
 ) -> Result<String> {
-  // let file = fs::File::open(script)?;
-  // let bit_update = 0o700;
-  // let mut permissions = file.metadata()?.permissions();
-  // permissions.set_mode(permissions.mode() | bit_update);
-  // file.set_permissions(permissions)?;
-
-  let mut cmd = Command::new(script)
+  if let Ok(mut cmd) = Command::new(script)
     .stdin(Stdio::piped())
     .stdout(Stdio::piped())
-    .spawn()?;
-  let cmd_stdin = cmd.stdin.as_mut().unwrap();
-  cmd_stdin.write_all(data)?;
-  let output = cmd.wait_with_output()?;
-  Ok(String::from_utf8(output.stdout).unwrap())
+    .spawn()
+  {
+    let cmd_stdin = cmd.stdin.as_mut().unwrap();
+    cmd_stdin.write_all(data)?;
+    let output = cmd.wait_with_output()?;
+    Ok(String::from_utf8(output.stdout).unwrap())
+  } else {
+    // TODO: This is temporary.  Hoist this so it kills all
+    // the processes, or has an ability to restart
+    // or something when the script is fixed.
+    println!("-------------------------------");
+    panic!(
+      "Could not run script: {}\nYou'll need to fix (maybe permissions?) it and restart\n\n-----------------------------------",
+      &script
+    );
+  }
 }
